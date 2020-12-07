ff46e133dff95072682b2c31d6a2c452b0c8cb4e
Dykann <dykann3@hotmail.fr>
2020-12-07 19:32:22 +0100

    Find other ways to push out the first letter when adding a new letter, the maximum numbers of characters (secretCode.length) of the array has already been reached

--- 12/main.js
+++ 12/main.js
@@ -4,10 +4,19 @@ const secretCode = "maman";
 window.addEventListener("keyup", (e) => {
   console.log(e.key);
   pressed.push(e.key);
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  //* pressed.splice(0, pressed.length - secretCode.length);
+  // if (pressed.length > secretCode.length) pressed.splice(0, 1);
+  if (pressed.length > secretCode.length) pressed.shift();
+
   if (pressed.join("").includes(secretCode)) {
     console.log("DING DONG!");
     cornify_add();
   }
   console.log(pressed);
 });
+
+// Urgent!
+
+//* pushing a new item when there are already 5 items in the array, it'll add 6 items in total to the pressed.length
+//*  which is why pressed.length - secretCode.length : 6-5 = 1 and so delete one item at the index 0 (the first item)
