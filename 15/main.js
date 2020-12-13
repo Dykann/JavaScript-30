75319aedc80f3bcd6e44988b4cb79821cf8b9d68
Dykann <dykann3@hotmail.fr>
2020-12-13 23:57:59 +0100

    Learn localStorage

---
+++ 15/main.js
@@ -0,0 +1,32 @@
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelectorAll(".plates");
+const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("input[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  this.reset();
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, index) => {
+      return `
+        <li>
+            <input type="checkbox" data-index=${index} id="item${index}" ${
+        plate.done ? "checked" : ""
+      }/>
+            <label for="item${index}">${plate.text}</label>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+addItems.addEventListener("submit", addItem);
